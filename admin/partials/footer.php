bc0254beac1c00af0c70ec0df2865b0f74894b8e
PinpinReagan <johnreaganpinpin013@gmail.com>
2024-11-22 06:59:14 +0800

    here is the attach subject

--- admin/partials/footer.php
+++ admin/partials/footer.php
@@ -1,5 +1,8 @@
-            </div>
-        </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>    
+<!-- Footer content -->
+<footer class="bg-dark text-white text-center py-3">
+    &copy; 2024 Your Application. All Rights Reserved.
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
